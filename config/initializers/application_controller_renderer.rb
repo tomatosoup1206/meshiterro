3e1cfa21a90815e402d356a01a09bbe45d52869d
tomatosoup1206 <kodama.tokiya.infratop@gmail.com>
2019-07-01 04:53:42 +0900

    finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '67b5c7715468e0e57074b53cb1ad61496f45f05880485f6cf753bb63944425b5e54043abc0851e8c8c4681d3b3e02043da3faf23b776ff7a032eb1da162628f8'
---
+++ db/migrate/20190630162142_add_proflie_image_id_to_users.rb
@@ -0,0 +1,5 @@
+class AddProflieImageIdToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :profile_image_id, :string
+  end
+end
